03acf1cdee75a0b9e24ca2c6d4fe6bd41ce72ece
Karolina Szymanska <karolinaszymanska899@gmail.com>
2024-10-03 20:10:04 +0200

    created a basic example with Three.js

--- main.js
+++ main.js
@@ -1,24 +1,77 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const canvas = document.getElementById("canvas");
 
-setupCounter(document.querySelector('#counter'))
+// 1. Scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color("#F0F0F0");
+
+// 2. Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 5;
+
+// 3. Object
+const geometry = new THREE.DodecahedronGeometry();
+const material = new THREE.MeshLambertMaterial({
+  color: "#468585",
+  emissive: "#468585",
+});
+const dodecahedron = new THREE.Mesh(geometry, material);
+
+const boxGeometry = new THREE.BoxGeometry(2, 0.1, 2);
+const boxMaterial = new THREE.MeshStandardMaterial({
+  color: "#B4B4B3",
+  emissive: "#B4B4B3",
+});
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+box.position.y = -1.5;
+
+scene.add(dodecahedron);
+scene.add(box);
+
+// 4. Light
+const light = new THREE.SpotLight(0x006769, 100);
+light.position.set(1, 1, 1);
+scene.add(light);
+
+// 5. Renderer
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+// 6. Add OrbitControls
+const controls = new OrbitControls(camera, renderer.domElement);
+// makes controls smoother:
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePan = true;
+
+// Add Animations
+function animate() {
+  requestAnimationFrame(animate);
+
+  dodecahedron.rotation.x += 0.01;
+  dodecahedron.rotation.y += 0.01;
+
+  box.rotation.y += 0.005;
+
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+// Handle window resizing
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / innerHeight;
+  //resets camera position:
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+animate();
