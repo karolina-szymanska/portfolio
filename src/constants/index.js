ddd85c08e8e0e963a644c5a5a567f103d132f96f
Karolina Szymanska <karolinaszymanska899@gmail.com>
2024-10-05 15:23:18 +0200

    added myProjects constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -55,3 +55,91 @@ export const calculateSizes = (isSmall, isMobile, isTablet) => {
       : [-13, -13, -10],
   };
 };
+
+export const myProjects = [
+  {
+    title: "Semester Project 2, Auction",
+    desc: "Auction is a website where users can add items to be bid on and bid on items other users have put up for auction. Users can get credits by selling items and use credit by buying items. Non-registered users can search through the listings, but only registered users can make bids on listings.",
+    subdesc: "Built with HTML5, Bootstrap, JavaScript and SASS.",
+    href: "https://auction-semester-project-2.netlify.app/",
+    // texture: "/textures/project/project1.mp4",
+    spotlight: "/assets/spotlight1.png",
+    tags: [
+      {
+        id: 1,
+        name: "HTML5",
+        path: "/assets/html5.svg",
+      },
+      {
+        id: 2,
+        name: "Bootstrap",
+        path: "/assets/bootstrap.svg",
+      },
+      {
+        id: 3,
+        name: "JavaScript",
+        path: "/assets/javascript.svg",
+      },
+      {
+        id: 4,
+        name: "Sass",
+        path: "/assets/sass.svg",
+      },
+    ],
+  },
+  {
+    title: "JavaScript CA, eCOM",
+    desc: "The Homepage has a list of all the products. The individual product page displays data for a single product. Cart page lists all of the chosen products as well as a total. Contact page ensures form validation.",
+    subdesc: "Built with React, React-Router-DOM, JavaScript and Zustand.",
+    href: "https://javascript-frameworks-react-ca.netlify.app/",
+    // texture: "/textures/project/project2.mp4",
+    spotlight: "/assets/spotlight2.png",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "React-Router-DOM",
+        path: "/assets/reactrouter.svg",
+      },
+      {
+        id: 3,
+        name: "JavaScript",
+        path: "/assets/javascript.svg",
+      },
+    ],
+  },
+  {
+    title: "Project Exam 2, Holidaze",
+    desc: "The two main aspects: the customer-facing side of the website where users can book holidays at a venue, and an admin-facing side of the website where users can register and manage venues and bookings at those venues.",
+    subdesc: "Built with Vite, React, React-Router-DOM and TailwindCSS.",
+    href: "https://project-exam-2-karolina-szymanska.netlify.app/",
+    // texture: "/textures/project/project3.mp4",
+    spotlight: "/assets/spotlight3.png",
+    tags: [
+      {
+        id: 1,
+        name: "Vite.js",
+        path: "/assets/vitejs.svg",
+      },
+      {
+        id: 2,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 3,
+        name: "React-Router-DOM",
+        path: "/assets/reactrouter.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/tailwindcss.png",
+      },
+    ],
+  },
+];
