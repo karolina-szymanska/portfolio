98a551bf14ed51e178dea3a8b9fc298025993590
Karolina Szymanska <karolinaszymanska899@gmail.com>
2024-10-06 15:52:33 +0200

    imported Experience section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./sections/Hero.jsx";
 import About from "./sections/About.jsx";
 import Projects from "./sections/Projects.jsx";
 import Clients from "./sections/Clients.jsx";
+import Experience from "./sections/Experience.jsx";
 import Contact from "./sections/Contact.jsx";
 import Footer from "./sections/Footer.jsx";
 
@@ -14,7 +15,7 @@ const App = () => {
       <About />
       <Projects />
       <Clients />
-
+      <Experience />
       <Contact />
       <Footer />
     </main>
