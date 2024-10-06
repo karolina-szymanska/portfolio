413523b59e92af30aef9dd5d7ad03b202153d596
Karolina Szymanska <karolinaszymanska899@gmail.com>
2024-10-06 22:24:38 +0200

    styling of position in OrbitControls

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -134,12 +134,12 @@ const Projects = () => {
             <directionalLight position={[10, 10, 5]} />
             <Center>
               <Suspense fallback={<CanvasLoader />}>
-                <group scale={9} position={[0, -3, 0]} rotation={[0, -0.1, 0]}>
+                <group scale={10} position={[0, -3, 0]} rotation={[0, 1, 0]}>
                   <DemoComputer />
                 </group>
               </Suspense>
             </Center>
-            <OrbitControls maxPolarAngle={Math.PI / 2} enableZoom={true} />
+            <OrbitControls maxPolarAngle={Math.PI / 2} enableZoom={false} />
           </Canvas>
         </div>
       </div>
