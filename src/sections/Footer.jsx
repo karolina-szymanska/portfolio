fef0187b3ace0604530a8ff57d38c1d24c68cddc
Karolina Szymanska <karolinaszymanska899@gmail.com>
2024-10-06 15:17:35 +0200

    added the section

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,3 +1,7 @@
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+
 const Footer = () => {
   return (
     <footer className="c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
@@ -9,21 +13,15 @@ const Footer = () => {
 
       <div className="flex gap-3">
         <div className="social-icon">
-          <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
+          <FaGithub className="w-1/2 h-1/2" />
         </div>
+
         <div className="social-icon">
-          <img
-            src="/assets/twitter.svg"
-            alt="twitter"
-            className="w-1/2 h-1/2"
-          />
+          <FaLinkedinIn className="w-1/2 h-1/2" />
         </div>
+
         <div className="social-icon">
-          <img
-            src="/assets/facebook.svg"
-            alt="facebook"
-            className="w-1/2 h-1/2"
-          />
+          <FaFacebook className="w-1/2 h-1/2" />
         </div>
       </div>
 
