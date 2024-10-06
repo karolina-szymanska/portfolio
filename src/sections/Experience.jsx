27edb791754106aec1543a0441dffdbd092d1ace
Karolina Szymanska <karolinaszymanska899@gmail.com>
2024-10-06 15:52:17 +0200

    created the section

---
+++ src/sections/Experience.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <section className="">
+      <h3 className="text-white">Experience</h3>
+    </section>
+  );
+};
+
+export default Experience;
